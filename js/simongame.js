f6a8224ecc9413f7a4d983d95ae08072e13c7716
Adriana Molina <moladriana@gmail.com>
2016-09-06 18:11:06 +0200

    Reset all variables when simon game is turned off, and remove all new styles

--- js/simongame.js
+++ js/simongame.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
-console.log("se ha cargado la página");
+
 var isTheGameOn = false;
 var playerTurn = false;
+//esta variable podría ir dentro de la función random
 var choices = ["green", "red", "yellow", "blue"];
 var randomSequence = [];
 var timesPlayer = 0;
 
+
+/////////// TURN ON AND OFF THE GAME
 $(".square").click(function() {
     if(!isTheGameOn) {
         isTheGameOn = true;
         $(".square").css("margin-left", "0.1em");
+        $(".counter p").css("color", "#f9320c");
     }
     else {
         isTheGameOn = false;
-        $(".square").css("margin-left", "-1.4em");
+        playerTurn = false;
+        randomSequence = [];
+        timesPlayer = 0;
+        $(".square, .counter p").removeAttr("style");
     }
 });
 
